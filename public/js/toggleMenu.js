b2682ea5e9d36ff735f138950cf95de1e2b2c899
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-02-03 16:40:01 -0500

    finished styling/functionality for mobile menu

--- public/js/toggleMenu.js
+++ public/js/toggleMenu.js
@@ -1,13 +1,18 @@
 const hamburgerBtn = document.querySelector('.hamburger')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuContent = document.querySelector('.menu-content')
-const menuItem = document.querySelector('.menu-item')
+const menuItems = document.querySelectorAll('.menu-item')
+const body = document.querySelector('body')
 
 hamburgerBtn.addEventListener('click', toggleMenu)
-menuItem.addEventListener('click', toggleMenu)
+
+for (i of menuItems) {
+   i.addEventListener('click', toggleMenu)
+}
 
 function toggleMenu() {
    hamburgerBtn.classList.toggle('is-active')
    mobileMenu.classList.toggle('is-active')
    menuContent.classList.toggle('is-active')
+   body.classList.toggle('no-scroll')
 }
