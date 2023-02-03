7cfbe8527d06327758ee33477ca4730aa4c40af0
Hannah Wohl-Machado <hannahmachado@yahoo.ca>
2023-02-03 08:41:25 -0500

    adding functionality to mobile menu

--- public/js/toggleMenu.js
+++ public/js/toggleMenu.js
@@ -1,7 +1,13 @@
 const hamburgerBtn = document.querySelector('.hamburger')
+const mobileMenu = document.querySelector('.mobile-menu')
+const menuContent = document.querySelector('.menu-content')
+const menuItem = document.querySelector('.menu-item')
 
 hamburgerBtn.addEventListener('click', toggleMenu)
+menuItem.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
    hamburgerBtn.classList.toggle('is-active')
+   mobileMenu.classList.toggle('is-active')
+   menuContent.classList.toggle('is-active')
 }
